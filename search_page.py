587fa1b162c48f0552a5c08ca6081032fc46116a
esrackmakli <68961319+esrackmakli@users.noreply.github.com>
2021-04-14 16:41:13 +0300

    Update search_page.py

--- search_page.py
+++ search_page.py
@@ -17,6 +17,7 @@ class SearchPage:
     def search_result(self):
         """
         Checks if there are any results for the searched word
+        
         """
         result = self.functions.exist_element(self.SEARCH_RESULT)
         assert result, "didn't found for searched word! "
@@ -24,6 +25,7 @@ class SearchPage:
     def second_page_click(self):
         """
         loads the second page on the search page and checks it
+        
         """
         self.functions.wait_for_element(self.SECOND_PAGE).click()
         is_second_page = self.functions.exist_element(self.IS_SECOND_PAGE)
@@ -32,6 +34,7 @@ class SearchPage:
     def third_product_click(self):
         """
         Clicks the third product on the second page and checks it
+        
         """
         self.functions.wait_for_elements(self.THIRD_PRODUCT)[2].click()
         third_product_clicked = self.functions.wait_for_element(self.THIRD_PRODUCT_CLICKED)
